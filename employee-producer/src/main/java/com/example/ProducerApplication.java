d9b1c6e79bc1ecb5b8e856fe1863670b6e0d4e66
deepikdm <deepikdevidm@gmail.com>
2020-01-24 16:18:21 +0530

    commit for hystrix

--- employee-producer/src/main/java/com/example/ProducerApplication.java
+++ employee-producer/src/main/java/com/example/ProducerApplication.java
@@ -2,13 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableDiscoveryClient
-public class SpringBootHelloWorldApplication {
+public class ProducerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+		SpringApplication.run(ProducerApplication.class, args);
 	}
 }
